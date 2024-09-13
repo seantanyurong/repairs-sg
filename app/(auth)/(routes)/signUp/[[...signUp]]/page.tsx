22aedcb4b84ad303844b1594bc890f21a7ea9339
Stanley Mak <stanleymaktx@gmail.com>
2024-09-13 21:22:39 +0800

    renamed pages

--- app/(auth)/(routes)/signUp/[[...signUp]]/page.tsx
+++ app/(auth)/(routes)/signUp/[[...signUp]]/page.tsx
@@ -1,6 +1,6 @@
 import { SignUp } from '@clerk/nextjs'
 
-export default function Register() {
+export default function SignUpPage() {
   return (
     <div className="flex justify-center items-center h-screen">
         <SignUp />
