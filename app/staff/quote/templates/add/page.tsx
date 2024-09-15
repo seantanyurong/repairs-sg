051808fdb21320cedf50f75c1193ac8ce30321d3
Nevan Ng <ngnevan@gmail.com>
2024-09-15 13:25:11 +0800

    feat(quote template): add quote template scaffold

---
+++ app/staff/quote/templates/add/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="flex flex-col gap-2">
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Add Quote Template
+      </h1>
+    </div>
+  );
+};
+
+export default page;
