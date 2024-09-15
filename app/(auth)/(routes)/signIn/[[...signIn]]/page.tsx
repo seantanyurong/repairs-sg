97c701b23bbae9e3c2131ab0996fa666cc68c9a2
Stanley Mak <stanleymaktx@gmail.com>
2024-09-15 20:38:40 +0800

    profile module with additional fields

--- app/(auth)/(routes)/signIn/[[...signIn]]/page.tsx
+++ app/(auth)/(routes)/signIn/[[...signIn]]/page.tsx
@@ -1,9 +1,9 @@
-import { SignIn } from '@clerk/nextjs'
+import { SignIn } from "@clerk/nextjs";
 
 export default function SignInPage() {
   return (
     <div className="flex justify-center items-center h-screen">
-        <SignIn />
-    </div>  
-  )
+      <SignIn />
+    </div>
+  );
 }
