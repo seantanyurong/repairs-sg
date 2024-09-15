c6279a90c49d0f1131c47c4ed187ee18639cf67e
Nevan Ng <ngnevan@gmail.com>
2024-09-15 13:25:28 +0800

    style(quote template): add styling for templates page

--- app/staff/quote/templates/page.tsx
+++ app/staff/quote/templates/page.tsx
@@ -1,24 +1,39 @@
 import { DataTable } from "@/components/ui/data-table";
 import React from "react";
 import { columns, QuoteTemplate } from "./columns";
+import { Button } from "@/components/ui/button";
+import { Template } from "@pdfme/common";
 
-const templates: QuoteTemplate[] = [];
+const templates: QuoteTemplate[] = [
+  {
+    id: "",
+    status: "active",
+    name: "Template 1",
+    pdfTemplate: {
+      id: "",
+      name: "",
+      content: "",
+    } as unknown as Template, //TODO: Remove mock data
+  },
+];
 
 const page = () => {
   return (
-    <>
-      <h1>Quote Templates</h1>
-      <p>
+    <div className="flex flex-col gap-2">
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Quote Templates
+      </h1>
+      <p className="leading-7 [&:not(:first-child)]:mt-6">
         Quote templates are used to generate PDFs for quotes. You can create
         multiple templates and select the one you want to use when creating a
         quote.{" "}
       </p>
-      <button>Create Template</button>
+      <Button className="self-end">Add Template</Button>
       <DataTable
         columns={columns}
         data={templates}
       />
-    </>
+    </div>
   );
 };
 
