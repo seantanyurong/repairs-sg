f1e8acce51ceabf99003c762f916fe0969a0084e
Sean Tan <sean.tan@sean.tan-GHKX6X1N9C>
2024-09-16 14:29:36 +0800

    feature: change header loc; import additional ui components

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,9 +1,31 @@
 import '../css/globals.css';
+import { ClerkProvider, SignInButton, SignedIn, SignedOut } from '@clerk/nextjs';
+import Link from 'next/link';
+import UserButtonCustom from './_components/account/userButtonCustom';
 
 export default async function PublicLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div>{children}</div>;
+  function Header() {
+    return (
+      <header style={{ display: 'flex', justifyContent: 'space-between', padding: 20 }}>
+        <Link href='/'>Repairs.sg</Link>
+        <SignedIn>
+          <UserButtonCustom />
+        </SignedIn>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+      </header>
+    );
+  }
+
+  return (
+    <ClerkProvider>
+      <Header />
+      {children}
+    </ClerkProvider>
+  );
 }
