47dff98fbd82b92fa8314b6398c96b12b8e388d4
Sean Tan <sean.tan@sean.tan-GHKX6X1N9C>
2024-09-16 21:25:17 +0800

    feature: add server and client validation for form

--- app/(staff-only)/staff/services/createService/page.tsx
+++ app/(staff-only)/staff/services/createService/page.tsx
@@ -1,13 +1,101 @@
-import { addService } from '@/lib/actions/services';
+'use client';
 
-export default async function CreateService() {
+import * as z from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { addService } from '@/lib/actions/services';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+// import { SelectValue, SelectTrigger, SelectContent, SelectItem, Select } from '@/components/ui/select';
+
+const formSchema = z.object({
+  name: z.string().min(1),
+  description: z.string().min(1),
+});
+
+export default function CreateService() {
+  const [message, setMessage] = useState('');
+  const [errors, setErrors] = useState({});
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+      description: '',
+    },
+  });
+
+  const onSubmit = async () => {
+    setMessage('');
+    setErrors({});
+    const result = await addService(form.getValues());
+    if (result?.errors) {
+      setMessage(result.message);
+      setErrors(result.errors);
+      return;
+    } else {
+      setMessage(result.message);
+      router.refresh();
+      form.reset(form.getValues());
+    }
+
+    console.log(form.getValues());
+  };
+
   return (
-    <form action={addService}>
-      <div>
-        <label>Name</label>
-        <input name='name' type='text' />
-      </div>
-      <button>Submit</button>
-    </form>
+    <Form {...form}>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          form.handleSubmit(onSubmit)();
+        }}
+        className='max-w-md w-full flex flex-col gap-4'>
+        <FormField
+          control={form.control}
+          name='name'
+          render={({ field }) => {
+            return (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input placeholder='Name' {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            );
+          }}
+        />
+        <FormField
+          control={form.control}
+          name='description'
+          render={({ field }) => {
+            return (
+              <FormItem>
+                <FormLabel>Description</FormLabel>
+                <FormControl>
+                  <Input placeholder='Description' {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            );
+          }}
+        />
+        <Button type='submit' className='w-full'>
+          Submit
+        </Button>
+        {message ? <h2>{message}</h2> : null}
+        {errors ? (
+          <div className='mb-10 text-red-500'>
+            {Object.keys(errors).map((key) => (
+              <p key={key}>{`${key}: ${errors[key as keyof typeof errors]}`}</p>
+            ))}
+          </div>
+        ) : null}
+      </form>
+    </Form>
   );
 }
--- lib/actions/services.tsx
+++ lib/actions/services.tsx
@@ -1,12 +1,32 @@
 'use server';
 
 import Service from '@/models/Service';
+import { z } from 'zod';
 
-const addService = async (service: FormData) => {
-  const name = service.get('name');
+const addService = async (service: {
+  name: string;
+  description: string;
+}): Promise<{ message: string; errors?: string | Record<string, unknown> }> => {
+  const serviceSchema = z.object({
+    name: z.string().min(1),
+    description: z.string().min(1),
+  });
 
-  const newService = new Service({ name });
+  const response = serviceSchema.safeParse({
+    name: service.name,
+    description: service.description,
+  });
+
+  if (!response.success) {
+    return { message: 'Error', errors: response.error.flatten().fieldErrors };
+  }
+
+  console.log(response.data);
+
+  const newService = new Service(response.data);
   newService.save();
+
+  return { message: 'Service added successfully' };
 };
 
 const getServices = async () => {
