ea7621c609acd9d4dbc8301bb1078db4b69f3a18
Stanley Mak <stanleymaktx@gmail.com>
2024-09-17 23:52:11 +0800

    renamed filenames

--- app/(public)/_components/account/tabs/phone/PhoneTab.tsx
+++ app/(public)/_components/account/tabs/phone/PhoneTab.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Divider from "@/components/ui/divider";
 
-import PhoneSection from "./sections/PhoneSectionD";
+import PhoneSection from "./sections/PhoneSection";
 
 export default function PhoneTab() {
   return (
