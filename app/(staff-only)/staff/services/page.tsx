52158d9c603db27c8de772c35f0e963a7f64f711
Sean Tan <sean.tan@sean.tan-GHKX6X1N9C>
2024-09-17 17:19:42 +0800

    refactor: card display into functions

--- app/(staff-only)/staff/services/page.tsx
+++ app/(staff-only)/staff/services/page.tsx
@@ -11,24 +11,84 @@ import ServiceRow from './_components/ServiceRow';
 export default async function Services() {
   const services = await getServices();
 
-  const serviceDisplay = () => {
+  const serviceDisplay = (status?: string) => {
     if (services.length === 0) {
       return <div>No services found</div>;
     }
+
+    if (status === 'all') {
+      return services.map((service) => {
+        return (
+          <ServiceRow
+            key={service._id.toString()}
+            id={service._id.toString()}
+            name={service.name}
+            status={service.status}
+            createdAt={service.createdAt.toString()}
+          />
+        );
+      });
+    }
+
+    // Filter by status
+    return services
+      .filter((service) => service.status.toLowerCase() === status)
+      .map((service) => {
+        return (
+          <ServiceRow
+            key={service._id.toString()}
+            id={service._id.toString()}
+            name={service.name}
+            status={service.status}
+            createdAt={service.createdAt.toString()}
+          />
+        );
+      });
+  };
+
+  const serviceCount = (status?: string) => {
+    if (status === 'all') {
+      return services.length;
+    }
+
+    return services.filter((service) => service.status.toLowerCase() === status).length;
+  };
+
+  const cardDisplay = (status?: string) => {
     return (
-      <>
-        {services.map((service) => {
-          return (
-            <ServiceRow
-              key={service._id.toString()}
-              id={service._id.toString()}
-              name={service.name}
-              status={service.status}
-              createdAt={service.createdAt.toString()}
-            />
-          );
-        })}
-      </>
+      <Card x-chunk='dashboard-06-chunk-0'>
+        <CardHeader>
+          <CardTitle>Services</CardTitle>
+          <CardDescription>Manage your services and edit their details.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className='hidden w-[100px] sm:table-cell'>
+                  <span className='sr-only'>Image</span>
+                </TableHead>
+                <TableHead>Name</TableHead>
+                <TableHead>Status</TableHead>
+                <TableHead className='hidden md:table-cell'>Created at</TableHead>
+                <TableHead>
+                  <span className='sr-only'>Actions</span>
+                </TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>{serviceDisplay(status)}</TableBody>
+          </Table>
+        </CardContent>
+        <CardFooter>
+          <div className='text-xs text-muted-foreground'>
+            Showing{' '}
+            <strong>
+              {serviceCount(status) === 0 ? '0' : '1'}-{serviceCount(status)}
+            </strong>{' '}
+            of <strong>{serviceCount(status)}</strong> services
+          </div>
+        </CardFooter>
+      </Card>
     );
   };
 
@@ -37,32 +97,13 @@ export default async function Services() {
       <div className='flex items-center'>
         <TabsList>
           <TabsTrigger value='all'>All</TabsTrigger>
-          {/* <TabsTrigger value='active'>Active</TabsTrigger>
+          <TabsTrigger value='active'>Active</TabsTrigger>
           <TabsTrigger value='draft'>Draft</TabsTrigger>
-          <TabsTrigger value='archived' className='hidden sm:flex'>
-            Archived
-          </TabsTrigger> */}
+          <TabsTrigger value='disabled' className='hidden sm:flex'>
+            Disabled
+          </TabsTrigger>
         </TabsList>
         <div className='ml-auto flex items-center gap-2'>
-          {/* <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant='outline' size='sm' className='h-8 gap-1'>
-                <ListFilter className='h-3.5 w-3.5' />
-                <span className='sr-only sm:not-sr-only sm:whitespace-nowrap'>Filter</span>
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align='end'>
-              <DropdownMenuLabel>Filter by</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuCheckboxItem checked>Active</DropdownMenuCheckboxItem>
-              <DropdownMenuCheckboxItem>Draft</DropdownMenuCheckboxItem>
-              <DropdownMenuCheckboxItem>Archived</DropdownMenuCheckboxItem>
-            </DropdownMenuContent>
-          </DropdownMenu> */}
-          {/* <Button size='sm' variant='outline' className='h-8 gap-1'>
-            <File className='h-3.5 w-3.5' />
-            <span className='sr-only sm:not-sr-only sm:whitespace-nowrap'>Export</span>
-          </Button> */}
           <Link href='/staff/services/create-service'>
             <Button size='sm' className='h-8 gap-1'>
               <PlusCircle className='h-3.5 w-3.5' />
@@ -71,37 +112,10 @@ export default async function Services() {
           </Link>
         </div>
       </div>
-      <TabsContent value='all'>
-        <Card x-chunk='dashboard-06-chunk-0'>
-          <CardHeader>
-            <CardTitle>Services</CardTitle>
-            <CardDescription>Manage your services and edit their details.</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <Table>
-              <TableHeader>
-                <TableRow>
-                  <TableHead className='hidden w-[100px] sm:table-cell'>
-                    <span className='sr-only'>Image</span>
-                  </TableHead>
-                  <TableHead>Name</TableHead>
-                  <TableHead>Status</TableHead>
-                  <TableHead className='hidden md:table-cell'>Created at</TableHead>
-                  <TableHead>
-                    <span className='sr-only'>Actions</span>
-                  </TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>{serviceDisplay()}</TableBody>
-            </Table>
-          </CardContent>
-          <CardFooter>
-            <div className='text-xs text-muted-foreground'>
-              Showing <strong>1-{services.length}</strong> of <strong>{services.length}</strong> services
-            </div>
-          </CardFooter>
-        </Card>
-      </TabsContent>
+      <TabsContent value='all'>{cardDisplay('all')}</TabsContent>
+      <TabsContent value='active'>{cardDisplay('active')}</TabsContent>
+      <TabsContent value='draft'>{cardDisplay('draft')}</TabsContent>
+      <TabsContent value='disabled'>{cardDisplay('disabled')}</TabsContent>
     </Tabs>
   );
 }
