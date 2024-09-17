a480e658f31ccda27fcd2aa4927a04536dc1515c
Sean Tan <sean.tan@sean.tan-GHKX6X1N9C>
2024-09-17 18:53:05 +0800

    feature: add edit functionality

---
+++ app/(staff-only)/staff/services/edit-service/[serviceId]/page.tsx
@@ -0,0 +1,17 @@
+import { getService } from '@/lib/actions/services';
+import EditServiceClient from './clientPage';
+
+export default async function EditService({ params }: { params: { serviceId: string } }) {
+  const service = await getService(params.serviceId);
+
+  return (
+    <EditServiceClient
+      service={{
+        _id: service._id.toString(),
+        name: service.name,
+        description: service.description,
+        status: service.status,
+      }}
+    />
+  );
+}
--- lib/actions/services.tsx
+++ lib/actions/services.tsx
@@ -3,6 +3,7 @@
 import Service from '@/models/Service';
 import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
+import { ObjectId } from 'mongodb';
 
 const addService = async (service: {
   name: string;
@@ -31,15 +32,49 @@ const addService = async (service: {
   return { message: 'Service added successfully' };
 };
 
+const updateService = async (service: {
+  _id: string;
+  name: string;
+  description: string;
+  status: string;
+}): Promise<{ message: string; errors?: string | Record<string, unknown> }> => {
+  const serviceSchema = z.object({
+    _id: z.string().min(1),
+    name: z.string().min(1),
+    description: z.string().min(1),
+    status: z.enum(['Draft', 'Active', 'Disabled']),
+  });
+
+  const response = serviceSchema.safeParse({
+    _id: service._id,
+    name: service.name,
+    description: service.description,
+    status: service.status,
+  });
+
+  if (!response.success) {
+    return { message: 'Error', errors: response.error.flatten().fieldErrors };
+  }
+
+  const filter = { _id: new ObjectId(response.data._id) };
+  const update = { name: response.data.name, description: response.data.description, status: response.data.status };
+  await Service.findOneAndUpdate(filter, update);
+  revalidatePath('/staff/services');
+
+  return { message: 'Service updated successfully' };
+};
+
 const deleteService = async (serviceId: string) => {
   await Service.findByIdAndDelete(serviceId);
-
-  // Revalidate the home page to remove the deleted post
   revalidatePath('/staff/services');
+};
+
+const getService = async (serviceId: string) => {
+  return Service.findById(serviceId);
 };
 
 const getServices = async () => {
   return Service.find();
 };
 
-export { addService, deleteService, getServices };
+export { addService, updateService, deleteService, getService, getServices };
