41d6f7f6c58e8f385552f53c27deb1d5055ca220
Nevan Ng <ngnevan@gmail.com>
2024-09-17 16:19:19 +0800

    feat(quote template): add server action

---
+++ lib/actions/quoteTemplates.ts
@@ -0,0 +1,34 @@
+"use server";
+
+import { z } from "zod";
+import QuoteTemplate from "@/models/QuoteTemplate";
+import { Template } from "@pdfme/common";
+
+const addQuoteTemplate = async (quoteTemplate: {
+  name: string;
+  pdfTemplate: Template;
+}): Promise<{
+  message: string;
+  errors?: string | Record<string, unknown>;
+}> => {
+  const quoteTemplateSchema = z.object({
+    name: z.string().min(1),
+    pdfTemplate: z.object({}),
+  });
+
+  const response = quoteTemplateSchema.passthrough().safeParse(quoteTemplate);
+
+  if (!response.success) {
+    return { message: "Error", errors: response.error.flatten().fieldErrors };
+  } else {
+    const newQuoteTemplate = new QuoteTemplate(response.data);
+    newQuoteTemplate.save();
+    return { message: "Quote Template added successfully" };
+  }
+};
+
+const getQuoteTemplates = async () => {
+  return QuoteTemplate.find();
+};
+
+export { addQuoteTemplate, getQuoteTemplates };
