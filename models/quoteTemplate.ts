dbb84d2628dec6b036e079265668595008f2937a
Nevan Ng <ngnevan@gmail.com>
2024-09-17 18:06:51 +0800

    feat(quote template): add timestamp to model

--- models/quoteTemplate.ts
+++ models/quoteTemplate.ts
@@ -1,20 +1,27 @@
 import mongoose from "mongoose";
 
-const quoteTemplateSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: { required: true, message: "Template Name is required" },
+const quoteTemplateSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: { required: true, message: "Template Name is required" },
+    },
+    status: {
+      type: String,
+      enum: ["Active", "Inactive"],
+      default: "Active",
+    },
+    pdfTemplate: {
+      type: Object,
+      required: { required: true, message: "PDF Template is required" },
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  status: {
-    type: String,
-    enum: ["active", "inactive"],
-    default: "active",
-  },
-  pdfTemplate: {
-    type: Object,
-    required: { required: true, message: "PDF Template is required" },
-  },
-});
+  { versionKey: false, timestamps: true }
+);
 
 export default mongoose.models.QuoteTemplate ||
   mongoose.model("QuoteTemplate", quoteTemplateSchema);
