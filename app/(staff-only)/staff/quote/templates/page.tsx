a32ef25e67586c41932ea4c687a3de6bde6ca375
Nevan Ng <ngnevan@gmail.com>
2024-09-18 11:13:59 +0800

    dummy page and remove console log

--- app/(staff-only)/staff/quote/templates/page.tsx
+++ app/(staff-only)/staff/quote/templates/page.tsx
@@ -14,7 +14,6 @@ import { columns } from "./_components/QuoteTemplateColumns";
 
 const page = async () => {
   const templates = await getQuoteTemplates();
-  console.log(templates);
 
   return (
     <div className="flex flex-col gap-2">
