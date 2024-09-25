67f80969cc0b75abf9ce7e40bf687f837532cbeb
Nevan Ng <ngnevan@gmail.com>
2024-09-25 17:30:05 +0800

    feat(quotation): view quotation

---
+++ app/(staff-only)/staff/quote/edit/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Page = () => {
+  return <div>Edit quote</div>;
+};
+
+export default Page;
--- app/(staff-only)/staff/quote/page.tsx
+++ app/(staff-only)/staff/quote/page.tsx
@@ -1,7 +1,50 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { DataTable } from "@/components/ui/data-table";
+import { PlusCircle } from "lucide-react";
+import Link from "next/link";
+import { quotationColumns } from "./edit/_components/QuotationColumns";
 
 const Page = () => {
-  return <div>Quote</div>;
+  return (
+    <div className="flex flex-col gap-2">
+      <h1 className="scroll-m-20 text-2xl font-extrabold tracking-tight lg:text-5xl">
+        Quotations
+      </h1>
+      <Link
+        className="self-end"
+        href="/staff/quote/edit"
+      >
+        <Button className="h-8 gap-1">
+          <PlusCircle className="h-3.5 w-3.5" />
+          <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+            Create Quotation
+          </span>
+        </Button>
+      </Link>
+      <Card x-chunk="dashboard-06-chunk-0">
+        <CardHeader>
+          <CardTitle>Quotations</CardTitle>
+          <CardDescription>
+            Manage your quotations and edit their details.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <DataTable
+            columns={quotationColumns}
+            data={[]}
+            noResultsMessage="No quotations found."
+          />
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default Page;
