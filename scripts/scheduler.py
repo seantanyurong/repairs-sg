86c33d113ed5591e14d59b2c9cf4f832128af07b
kkinmunn <e0725374@u.nus.edu>
2024-09-29 00:36:42 +0800

    Feature: Added Graphs For Job Distributions

--- scripts/scheduler.py
+++ scripts/scheduler.py
@@ -4,13 +4,19 @@ import os
 
 def run_analytics_notebook():
     notebook_path = '../notebook/analytics.ipynb'
-    os.system(f"jupyter nbconvert --to notebook --execute --inplace {notebook_path}")
-    print("Analytics Notebook Executed and JSON Files Updated.")
+    exit_status = os.system(f"jupyter nbconvert --to notebook --execute --inplace {notebook_path}")
+    if exit_status == 0:
+        print("Analytics Notebook Executed and JSON Files Updated.")
+    else:
+        print("Failed To Execute Analytics Notebook. Please Check For Errors.")
 
 # def run_modelling_notebook():
 #     notebook_path = '../notebook/modelling.ipynb'
-#     os.system(f"jupyter nbconvert --to notebook --execute --inplace {notebook_path}")
-#     print("Modelling Notebook Executed and JSON Files Updated.")
+#     exit_status = os.system(f"jupyter nbconvert --to notebook --execute --inplace {notebook_path}")
+    # if exit_status == 0:
+    #     print("Modelling Notebook Executed and JSON Files Updated.")
+    # else:
+    #     print("Failed To Execute Modelling Notebook. Please Check For Errors.")
 
 # Schedule Analytics Notebook To Run Daily At 4 AM
 schedule.every().day.at("04:00").do(run_analytics_notebook)
