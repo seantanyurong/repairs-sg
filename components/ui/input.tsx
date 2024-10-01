2b7af07ade77902777db34e139a285339fbc8858
Sean Tan <seantanyurong@gmail.com>
2024-10-01 10:52:03 +0800

    fix: build bugs

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+// eslint-disable-next-line
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
