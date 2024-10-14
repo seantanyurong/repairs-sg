6c43b69df4226cb11dd58f0459b330ef32fd9419
Grace Yong <grace.yong.xue.min@gmail.com>
2024-10-14 11:38:35 +0800

    search invoice

--- app/(protected)/staff/invoices/page.tsx
+++ app/(protected)/staff/invoices/page.tsx
@@ -1,13 +1,7 @@
-import Link from 'next/link';
-import { createClerkClient } from '@clerk/nextjs/server';
-import { getInvoices } from '@/lib/actions/invoices';
-import { getPayments } from '@/lib/actions/payments';
-import { PlusCircle } from 'lucide-react';
-import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
-import { Table, TableBody, TableHead, TableHeader, TableRow } from '@/components/ui/table';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import InvoiceRow from './_components/InvoiceRow';
+import { createClerkClient } from "@clerk/nextjs/server";
+import { getInvoices } from "@/lib/actions/invoices";
+import { getPayments } from "@/lib/actions/payments";
+import Invoices from "./_components/Invoices";
 
 interface User {
   id: string;
@@ -22,17 +16,48 @@ interface CustomerMap {
   };
 }
 
-export default async function Invoices() {
-  // Fetch Payment
+export default async function InvoicesPage() {
   const payment = await getPayments();
-  console.log(payment);
+  console.log("payment", payment);
 
-  // Fetch Invoice
+  // Fetch Invoices
   const invoices = await getInvoices();
-  console.log(invoices)
+  // console.log("invoices", invoices);
+
+  // TODO: align with KM
+  // Provide a default date if date fields are missing or invalid
+  const defaultDate = new Date("1970-01-01T00:00:00Z").toISOString(); // Default date if missing
+
+  // Convert Date objects to ISO strings
+  const serializeInvoice = (invoice: any) => {
+    // Convert the Mongoose document to a plain object
+    const plainInvoice = invoice.toObject();
+
+    return {
+      ...plainInvoice,
+      // Ensure _id and dates are serialized to strings
+      _id: plainInvoice._id.toString(),
+      dateIssued: plainInvoice.dateIssued
+        ? plainInvoice.dateIssued.toISOString()
+        : defaultDate,
+      dateDue: plainInvoice.dateDue
+        ? plainInvoice.dateDue.toISOString()
+        : defaultDate,
+      createdAt: plainInvoice.createdAt
+        ? plainInvoice.createdAt.toISOString()
+        : defaultDate,
+      updatedAt: plainInvoice.updatedAt
+        ? plainInvoice.updatedAt.toISOString()
+        : defaultDate,
+    };
+  };
+  const serializedInvoices = invoices.map(serializeInvoice);
+  // console.log("serializedInvoices", serializedInvoices);
 
   // Fetch Customers
-  const custClerk = createClerkClient({ secretKey: process.env.CUSTOMER_CLERK_SECRET_KEY });
+  const custClerk = createClerkClient({
+    secretKey: process.env.CUSTOMER_CLERK_SECRET_KEY,
+  });
   const customers = await custClerk.users.getUserList();
   const customerMap: CustomerMap = {};
   customers.data.forEach((user: User) => {
@@ -41,124 +66,9 @@ export default async function Invoices() {
       lastName: user.lastName || "",
     };
   });
-
-  const invoiceDisplay = (validityStatus?: string) => {
-    if (validityStatus === 'all') {
-      return invoices.map((invoice) => {
-        const customer = customerMap[invoice.customer];
-        const fullName = customer ? `${customer.firstName} ${customer.lastName}` : 'Unknown';
-
-        return (
-          <InvoiceRow
-            key={invoice.invoiceId.toString()}
-            invoiceId={invoice.invoiceId.toString()}
-            dateIssued={invoice.dateIssued.toString()}
-            customer={fullName}
-            totalAmount={invoice.totalAmount.toString()}
-            lineItems={invoice.lineItems}
-            paymentStatus={invoice.paymentStatus}
-            validityStatus={invoice.validityStatus}
-            paymentMethod={invoice.payments[0]?.paymentMethod}
-          />
-        );
-      });
-    }
-
-    return invoices
-      .filter((invoice) => invoice.validityStatus === validityStatus)
-      .map((invoice) => {
-        const customer = customerMap[invoice.customer];
-        const fullName = customer ? `${customer.firstName} ${customer.lastName}` : 'Unknown';
-
-        return (
-          <InvoiceRow
-          key={invoice.invoiceId.toString()}
-          invoiceId={invoice.invoiceId.toString()}
-          dateIssued={invoice.dateIssued.toString()}
-          customer={fullName}
-          totalAmount={invoice.totalAmount.toString()}
-          lineItems={invoice.lineItems}
-          paymentStatus={invoice.paymentStatus}
-          validityStatus={invoice.validityStatus}
-          paymentMethod={invoice.payments[0]?.paymentMethod}
-          />
-        );
-      });
-  };
-
-  const invoiceCount = (validityStatus?: string) => {
-    if (validityStatus === 'all') {
-      return invoices.length;
-    }
-
-    return invoices.filter((invoice) => invoice.validityStatus === validityStatus).length;
-  };
-
-  const cardDisplay = (validityStatus?: string) => {
-    if (invoices.length === 0) {
-      return <div className='mt-4'>No Invoices Found</div>;
-    }
-
-    return (
-      <Card x-chunk='dashboard-06-chunk-0'>
-        <CardHeader>
-          <CardTitle>Invoices</CardTitle>
-          <CardDescription>Manage Customer Invoices and Edit Details.</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead>Invoice</TableHead>
-                <TableHead>Date</TableHead>
-                <TableHead>Customer</TableHead>
-                <TableHead>Amount</TableHead>
-                <TableHead>Line Items</TableHead>
-                <TableHead>Payment Status</TableHead>
-                <TableHead>Payment Method</TableHead>
-                <TableHead>
-                  <span className='sr-only'>Actions</span>
-                </TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>{invoiceDisplay(validityStatus)}</TableBody>
-          </Table>
-        </CardContent>
-        <CardFooter>
-          <div className='text-xs text-muted-foreground'>
-            Showing{' '}
-            <strong>
-              {invoiceCount(validityStatus) === 0 ? '0' : '1'}-{invoiceCount(validityStatus)}
-            </strong>{' '}
-            of <strong>{invoiceCount(validityStatus)}</strong> Invoices
-          </div>
-        </CardFooter>
-      </Card>
-    );
-  };
+  // console.log("custMap", customerMap);
 
   return (
-    <Tabs defaultValue='all'>
-      <div className='flex items-center'>
-        <TabsList>
-          <TabsTrigger value='all'>All</TabsTrigger>
-          <TabsTrigger value='active'>Active</TabsTrigger>
-          <TabsTrigger value='draft'>Draft</TabsTrigger>
-          <TabsTrigger value='void' className='hidden sm:flex'>Void</TabsTrigger>
-        </TabsList>
-        <div className='ml-auto flex items-center gap-2'>
-          <Link href='/staff/invoices/create-invoice'>
-            <Button size='sm' className='h-8 gap-1'>
-              <PlusCircle className='h-3.5 w-3.5' />
-              <span className='sr-only sm:not-sr-only sm:whitespace-nowrap'>Create Invoices</span>
-            </Button>
-          </Link>
-        </div>
-      </div>
-      <TabsContent value='all'>{cardDisplay('all')}</TabsContent>
-      <TabsContent value='active'>{cardDisplay('active')}</TabsContent>
-      <TabsContent value='draft'>{cardDisplay('draft')}</TabsContent>
-      <TabsContent value='void'>{cardDisplay('void')}</TabsContent>
-    </Tabs>
+    <Invoices initialInvoices={serializedInvoices} customerMap={customerMap} />
   );
 }
--- lib/actions/invoices.tsx
+++ lib/actions/invoices.tsx
@@ -1,20 +1,20 @@
-'use server'
+"use server";
 
-import Invoice from '@/models/Invoice';
-import mongoose from 'mongoose';
-import { z } from 'zod';
-import { revalidatePath } from 'next/cache';
+import Invoice from "@/models/Invoice";
+import mongoose from "mongoose";
+import { z } from "zod";
+import { revalidatePath } from "next/cache";
 
 const fieldFriendlyNames: Record<string, string> = {
-  invoiceId: 'Invoice ID',
-  lineItems: 'Line Items',
-  dateIssued: 'Issued Date',
-  dateDue: 'Due Date',
-  totalAmount: 'Total Amount',
-  remainingAmount: 'Remaining Amount',
-  paymentStatus: 'Payment Status',
-  validityStatus: 'Validity Status',
-  publicNote: 'Note',
+  invoiceId: "Invoice ID",
+  lineItems: "Line Items",
+  dateIssued: "Issued Date",
+  dateDue: "Due Date",
+  totalAmount: "Total Amount",
+  remainingAmount: "Remaining Amount",
+  paymentStatus: "Payment Status",
+  validityStatus: "Validity Status",
+  publicNote: "Note",
 };
 
 const addInvoice = async (invoice: {
@@ -30,23 +30,30 @@ const addInvoice = async (invoice: {
 }): Promise<{ message: string; errors?: string | Record<string, unknown> }> => {
   const invoiceSchema = z.object({
     invoiceId: z.number(),
-    lineItems: z.array(z.string()).nonempty("Line Items Should Have At Least 1 Item!"),
-    dateIssued: z.date().refine(date => date instanceof Date && !isNaN(date.getTime()), {
-      message: "Invalid Date",
-    }),
-    dateDue: z.date().refine(date => date instanceof Date && !isNaN(date.getTime()), {
-      message: "Invalid Date",
-    }).refine(date => date instanceof Date && date > new Date(), {
-      message: "Date Must Be In The Future!",
-    }),
+    lineItems: z
+      .array(z.string())
+      .nonempty("Line Items Should Have At Least 1 Item!"),
+    dateIssued: z
+      .date()
+      .refine((date) => date instanceof Date && !isNaN(date.getTime()), {
+        message: "Invalid Date",
+      }),
+    dateDue: z
+      .date()
+      .refine((date) => date instanceof Date && !isNaN(date.getTime()), {
+        message: "Invalid Date",
+      })
+      .refine((date) => date instanceof Date && date > new Date(), {
+        message: "Date Must Be In The Future!",
+      }),
     totalAmount: z.number().min(0.01, {
       message: "Total Amount Must Be Greater Than 0!",
     }),
     remainingDue: z.number().min(0, {
       message: "Remaining Due Cannot Be Negative!",
     }),
-    paymentStatus: z.enum(['Unpaid', 'Paid']),
-    validityStatus: z.enum(['Draft', 'Active', 'Void']),
+    paymentStatus: z.enum(["Unpaid", "Paid"]),
+    validityStatus: z.enum(["Draft", "Active", "Void"]),
     publicNote: z.string().max(500),
   });
 
@@ -64,28 +71,34 @@ const addInvoice = async (invoice: {
 
   console.log(response.data);
   if (!response.success) {
-    return { message: '', errors: response.error.flatten().fieldErrors };
+    return { message: "", errors: response.error.flatten().fieldErrors };
   }
 
   try {
     const newInvoice = new Invoice(response.data);
     await newInvoice.save();
 
-    return { message: 'Invoice Added Successfully' };
+    return { message: "Invoice Added Successfully" };
   } catch (error: unknown) {
     if (error instanceof mongoose.Error.ValidationError && error.errors) {
       // Mongoose validation errors (including unique-validator errors)
-      const mongooseErrors = Object.keys(error.errors).reduce((acc, key) => {
-        const friendlyKey = fieldFriendlyNames[key] || key;
-        const errorMessage = error.errors[key].message.replace(key, friendlyKey);
-        acc[friendlyKey] = [errorMessage];
-        return acc;
-      }, {} as Record<string, string[]>);
+      const mongooseErrors = Object.keys(error.errors).reduce(
+        (acc, key) => {
+          const friendlyKey = fieldFriendlyNames[key] || key;
+          const errorMessage = error.errors[key].message.replace(
+            key,
+            friendlyKey,
+          );
+          acc[friendlyKey] = [errorMessage];
+          return acc;
+        },
+        {} as Record<string, string[]>,
+      );
 
-      return { message: 'Validation Error', errors: mongooseErrors };
+      return { message: "Validation Error", errors: mongooseErrors };
     }
 
-    return { message: 'An Unexpected Error Occurred' };
+    return { message: "An Unexpected Error Occurred" };
   }
 };
 
@@ -102,23 +115,30 @@ const updateInvoice = async (invoice: {
 }): Promise<{ message: string; errors?: string | Record<string, unknown> }> => {
   const invoiceSchema = z.object({
     invoiceId: z.number(),
-    lineItems: z.array(z.string()).nonempty("Line Items Should Have At Least 1 Item!"),
-    dateIssued: z.date().refine(date => date instanceof Date && !isNaN(date.getTime()), {
-      message: "Invalid Date",
-    }),
-    dateDue: z.date().refine(date => date instanceof Date && !isNaN(date.getTime()), {
-      message: "Invalid Date",
-    }).refine(date => date instanceof Date && date > new Date(), {
-      message: "Date Must Be In The Future!",
-    }),
+    lineItems: z
+      .array(z.string())
+      .nonempty("Line Items Should Have At Least 1 Item!"),
+    dateIssued: z
+      .date()
+      .refine((date) => date instanceof Date && !isNaN(date.getTime()), {
+        message: "Invalid Date",
+      }),
+    dateDue: z
+      .date()
+      .refine((date) => date instanceof Date && !isNaN(date.getTime()), {
+        message: "Invalid Date",
+      })
+      .refine((date) => date instanceof Date && date > new Date(), {
+        message: "Date Must Be In The Future!",
+      }),
     totalAmount: z.number().min(0.01, {
       message: "Total Amount Must Be Greater Than 0!",
     }),
     remainingDue: z.number().min(0, {
       message: "Remaining Due Cannot Be Negative!",
     }),
-    paymentStatus: z.enum(['Unpaid', 'Paid']),
-    validityStatus: z.enum(['Draft', 'Active', 'Void']),
+    paymentStatus: z.enum(["Unpaid", "Paid"]),
+    validityStatus: z.enum(["Draft", "Active", "Void"]),
     publicNote: z.string().max(500),
   });
 
@@ -136,11 +156,11 @@ const updateInvoice = async (invoice: {
 
   console.log(response.data);
   if (!response.success) {
-    return { message: '', errors: response.error.flatten().fieldErrors };
+    return { message: "", errors: response.error.flatten().fieldErrors };
   }
 
   const filter = { invoiceId: response.data.invoiceId };
-  const update = { 
+  const update = {
     invoiceId: response.data.invoiceId,
     lineItems: response.data.lineItems,
     dateIssued: response.data.dateIssued,
@@ -151,26 +171,32 @@ const updateInvoice = async (invoice: {
     validityStatus: response.data.validityStatus,
     publicNote: response.data.publicNote,
   };
-  const context = { runValidators: true, context: 'query' };
+  const context = { runValidators: true, context: "query" };
 
   try {
     await Invoice.findOneAndUpdate(filter, update, context);
-    revalidatePath('/staff/invoices');
-    return { message: 'Invoices Updated Successfully' };
+    revalidatePath("/staff/invoices");
+    return { message: "Invoices Updated Successfully" };
   } catch (error: unknown) {
     if (error instanceof mongoose.Error.ValidationError && error.errors) {
       // Mongoose validation errors (including unique-validator errors)
-      const mongooseErrors = Object.keys(error.errors).reduce((acc, key) => {
-        const friendlyKey = fieldFriendlyNames[key] || key;
-        const errorMessage = error.errors[key].message.replace(key, friendlyKey);
-        acc[friendlyKey] = [errorMessage];
-        return acc;
-      }, {} as Record<string, string[]>);
+      const mongooseErrors = Object.keys(error.errors).reduce(
+        (acc, key) => {
+          const friendlyKey = fieldFriendlyNames[key] || key;
+          const errorMessage = error.errors[key].message.replace(
+            key,
+            friendlyKey,
+          );
+          acc[friendlyKey] = [errorMessage];
+          return acc;
+        },
+        {} as Record<string, string[]>,
+      );
 
-      return { message: 'Validation Error', errors: mongooseErrors };
+      return { message: "Validation Error", errors: mongooseErrors };
     }
 
-    return { message: 'An Unexpected Error Occurred' };
+    return { message: "An Unexpected Error Occurred" };
   }
 };
 
@@ -179,15 +205,12 @@ const getInvoice = async (invoiceId: number) => {
 };
 
 const getInvoices = async () => {
-  const invoices = await Invoice
-    .find()
-    .populate('payments')
+  const invoices = await Invoice.find()
+    .populate("payments")
     .sort({ invoiceId: -1 })
     .limit(20)
-    .exec()
-
+    .exec();
   return invoices;
-  
 };
 
 export { addInvoice, updateInvoice, getInvoice, getInvoices };
