15505a193725a6c2c85680aea1b41860e2530753
Nevan Ng <ngnevan@gmail.com>
2024-10-20 23:28:30 +0800

    fix: use inputs directly

--- app/(protected)/staff/invoices/view-invoice/[invoiceId]/page.tsx
+++ app/(protected)/staff/invoices/view-invoice/[invoiceId]/page.tsx
@@ -7,78 +7,11 @@ import { /*getInputFromTemplate,*/ Template } from "@pdfme/common";
 import InvoiceViewerClient from "./InvoiceViewerClient";
 import dayjs from "dayjs";
 
-const populateTemplate = (
-  oldTemplate: Template,
-  invoice: {
-    invoiceId: number;
-    dateIssued: string | number | Date | dayjs.Dayjs | null | undefined;
-    totalAmount: number;
-    lineItems: Array<string>;
-    invoiceTemplate: string;
-    qrCode: string;
-    customer: string;
-    // templateInputs: { [x: string]: string };
-  }
-): Template => {
-  const updatedSchema = oldTemplate.schemas[0].map((field) => {
-    switch (field.name) {
-      case "invoice_no":
-        return {
-          ...field,
-          content: `Invoice #${invoice.invoiceId}`,
-        };
-      case "invoice_date":
-        return {
-          ...field,
-          content: dayjs(invoice.dateIssued).format("DD/MM/YYYY"),
-        };
-      case "line_items":
-        return {
-          ...field,
-          content: JSON.stringify(invoice.lineItems),
-        };
-      case "total_amount":
-        return {
-          ...field,
-          content: invoice.totalAmount,
-        };
-      case "subtotal":
-        return {
-          ...field,
-          content: invoice.totalAmount,
-        };
-      case "qrcode":
-        return {
-          ...field,
-          content: btoa(invoice.qrCode),
-        };
-      // default: {
-      //   const content = invoice.templateInputs[field.name];
-      //   if (content) {
-      //     return {
-      //       ...field,
-      //       content,
-      //     };
-      //   }
-      //   return field;
-      // }
-    }
-  });
-  return {
-    ...oldTemplate,
-    schemas: [updatedSchema],
-  };
-};
-
 const ViewInvoice = async ({ params }: { params: { invoiceId: string } }) => {
   const invoice = await getInvoice(Number(params.invoiceId));
   console.log(invoice);
-  const invoiceTemplate = JSON.parse(await getOneQuoteTemplate(invoice.invoiceTemplate));
-  console.log(invoiceTemplate);
-
-  const updatedInvoiceTemplate = populateTemplate(
-    invoiceTemplate.pdfTemplate,
-    invoice
+  const invoiceTemplate = JSON.parse(
+    await getOneQuoteTemplate(invoice.invoiceTemplate)
   );
 
   const customer = invoice.customer
@@ -97,15 +30,15 @@ const ViewInvoice = async ({ params }: { params: { invoiceId: string } }) => {
         phone: "NA",
       };
 
-    const inputs = [
-      {
-        invoice_no: `Invoice #${invoice.invoiceId}`,
-        invoice_date: dayjs(invoice.dateIssued).format("DD/MM/YYYY"),
-        line_items: JSON.stringify(invoice.lineItems),
-        totalAmount: invoice.totalAmount.toString(),
-        qrCode: invoice.qrCode,
-      },
-    ];
+  const inputs = [
+    {
+      invoice_no: `Invoice #${invoice.invoiceId}`,
+      invoice_date: dayjs(invoice.dateIssued).format("DD/MM/YYYY"),
+      line_items: JSON.stringify(invoice.lineItems),
+      total_amount: invoice.totalAmount.toString(),
+      qrcode: invoice.qrCode,
+    },
+  ];
 
   return (
     <>
@@ -115,9 +48,11 @@ const ViewInvoice = async ({ params }: { params: { invoiceId: string } }) => {
         </h2>
       </div>
       <div className="flex justify-center items-center w-full min-h-screen">
-        <div className="flex lg:flex-row flex-col gap-2 w-3/4 h-auto lg:w-2/3"> {/*h-dvh*/}
+        <div className="flex lg:flex-row flex-col gap-2 w-3/4 h-auto lg:w-2/3">
+          {" "}
+          {/*h-dvh*/}
           <InvoiceViewerClient
-            template={updatedInvoiceTemplate}
+            template={invoiceTemplate.pdfTemplate}
             inputs={inputs}
           />
         </div>
