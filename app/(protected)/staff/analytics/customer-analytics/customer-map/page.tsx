47091413c7dc1511c0ff1bf9bdd824578aab0747
kkinmunn <e0725374@u.nus.edu>
2024-10-31 17:06:53 +0800

    Feature: Add Customer Map

--- app/(protected)/staff/analytics/customer-analytics/customer-map/page.tsx
+++ app/(protected)/staff/analytics/customer-analytics/customer-map/page.tsx
@@ -1,10 +1,14 @@
-'use client';
-
 export default function CustomerMap() {
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Customer Map</h1>
-      <p>Default Page</p>
+    <div>
+      <h1 className="text-2xl font-bold mb-4 md:text-3xl">Customer Interactive Map</h1>
+      <iframe 
+        src="/analytics/customer/customer-map.html" 
+        width="100%" 
+        height="500px" 
+        style={{ border: 'none' }} 
+        title="Map"
+      ></iframe>
     </div>
   );
 }
